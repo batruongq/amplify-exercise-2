ff30e2b7c5b36efd914810656c6eba546b7a8ba4
Ba Thi Truong <ba.truongthi@asnet.com.vn>
2022-06-05 15:06:01 +0700

    Amplify auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyexercise2": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
